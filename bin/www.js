73f20827028b6faa82096035d8f7741245e04228
SerhiiPitetsky <serhii.pitetsky@gmail.com>
2017-01-18 02:23:58 +0200

    server

---
+++ bin/www.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
+
+var http = require('http');
+var app = require('../app');
+var debug = require('debug')('express-raw:server');
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * Security measures
+ */
+app.disable('x-powered-by');
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('listening', () => console.log('Server started listening on ' + port));
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
